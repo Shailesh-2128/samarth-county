9c5fa283bed9afc356b45a7e2820b0b3d30a1539
Shailesh-2128 <shaileshjiwana15219@gmail.com>
2025-11-24 10:11:40 +0530

    intial commit

---
+++ js/svg-animation.js
@@ -0,0 +1,81 @@
+lottie.loadAnimation({
+        container: document.getElementById('crossfit'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'animations/Cardio Workout.json'
+    });
+
+lottie.loadAnimation({
+        container: document.getElementById('kidsplay'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'animations/Parkanim.json'
+    });
+
+lottie.loadAnimation({
+        container: document.getElementById('templeZone'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'animations/yoga_black.json'
+    }); 
+    
+    
+lottie.loadAnimation({
+        container: document.getElementById('barbeque'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'animations/barbecue_on_a_stick.json'
+    });     
+ 
+lottie.loadAnimation({
+        container: document.getElementById('library'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'animations/girl_with_books.json'
+    });       
+ 
+lottie.loadAnimation({
+        container: document.getElementById('electric'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'animations/ev-charging.json'
+    });        
+
+    seniorCitizen
+
+lottie.loadAnimation({
+        container: document.getElementById('seniorCitizen'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'animations/old-lady-and-woman.json'
+    });  
+lottie.loadAnimation({
+        container: document.getElementById('basketball'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'animations/sportbank-basketball.json'
+    });
+ 
+lottie.loadAnimation({
+        container: document.getElementById('cctv'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'animations/cctv-error.json'
+    }); 
+    
+lottie.loadAnimation({
+        container: document.getElementById('eco-friendly'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'animations/eco white.json'
+    });     
